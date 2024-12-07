b7e924ec3aa9465b301aa427692b64ebfd48f27e
front-endapps <website_dev@yahoo.com>
2024-12-07 10:05:38 +0200

    project

---
+++ src/web-components.js
@@ -0,0 +1,26 @@
+import { LitElement, html, css } from 'lit';
+import './components/HeaderComponent.js';
+import './components/FooterComponent.js';
+import './components/MainSection.js';
+
+const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
+
+class WebComponents extends LitElement {
+  static properties = {
+    header: { type: String },
+  }
+
+  constructor() {
+    super();
+    this.header = 'Lion lit app';
+  }
+
+  render() {
+    return html`
+    <header-component></header-component>
+    <main-section></main-section>
+    <footer-component></footer-component>
+    `;
+  }
+}
+customElements.define('web-components', WebComponents);
